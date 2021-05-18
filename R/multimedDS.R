1ce1bb14e865b3ab67474946930df46a88e546c4
davraam <avraam.demetris@gmail.com>
2021-05-19 00:13:31 +0300

    adding headers

--- R/multimedDS.R
+++ R/multimedDS.R
@@ -1,4 +1,35 @@
-multimedDS <- function(outcome=outcome.name, med.main=med.main.name, med.alt = NULL, treat=treat.name, covariates.transmit = NULL, data=data.name, sims=1000){
+#'
+#' @title Estimation and Sensitivity Analysis for Multiple Causal Mechanisms
+#' @description This function is similar to R function \code{multimed} from the 
+#' \code{mediation} package.
+#' @details The function 'multimed' is used for causal mediation analysis when
+#' post-treatment mediator-outcome confounders, or alternative mediators causally
+#' preceding the mediator of interest, exist in the hypothesized causal mechanisms.
+#' It estimates the average causal mediation effects (indirect effects) and the
+#' average direct effects under the homogeneous interaction assumption based on a
+#' varying-coefficient linear structural equation model. The function also performs
+#' sensitivity analysis with respect to the violation of the homogenous interaction
+#' assumption. The function can be used for both the single experiment design and
+#' the parallel design.
+#' @param outcome a string character, the name of the outcome variable in 'data'.
+#' @param med.main a string character, the name of the mediator of interest. Under
+#' the parallel design this is the only mediator variable used in the estimation.
+#' @param med.alt vector of character strings indicating the names of the 
+#' post-treatment confounders, i.e., the alternative mediators affecting both the
+#' main mediator and outcome. Not needed for the parallel design.
+#' @param treat a string character, the name of the treatment variable in 'data'.
+#' @param covariates vector of character strings representing the names of the
+#' pre-treatment covariates. Cannot be used for the parallel design.
+#' @param data a string character, the name of data frame containing all the 
+#' above variables.
+#' @param sims a number of bootstrap samples used for the calculation of 
+#' confidence intervals.
+#' @return a summary table of the object of class 'multimed'
+#' @author Demetris Avraam, for DataSHIELD Development Team
+#' @export
+#'
+multimedDS <- function(outcome=outcome.name, med.main=med.main.name, med.alt = NULL, treat=treat.name, 
+                       covariates.transmit = NULL, data=data.name, sims=1000){
   
   data <- eval(parse(text=data), envir = parent.frame())
   
--- R/neWeightDS.R
+++ R/neWeightDS.R
@@ -1,3 +1,15 @@
+#'
+#' @title Expand the dataset and calculate ratio-of-mediator probability weights
+#' @description This function is similar to R function \code{neWeight} from the 
+#' \code{mmedflex} package.
+#' @details The function 'neWeight' both expands the data along hypothetical exposure
+#' values and calculates ratio-of-mediator probability weights.
+#' @param object a string character, the name of an object used to select a method.
+#' @return a data frame of class c("data.frame", "expData", "weightData") is assigned at
+#' the server-side of each study.
+#' @author Demetris Avraam, for DataSHIELD Development Team
+#' @export
+#'
 neWeightDS <- function(object){
   
   object <- eval(parse(text=object), envir = parent.frame())
@@ -7,4 +19,4 @@ neWeightDS <- function(object){
   return(out)
   
 }
-# ASSIIGN FUNCTION
+# ASSIGN FUNCTION
