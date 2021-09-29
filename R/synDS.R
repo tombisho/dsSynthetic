df8b5df1250bf09d0a3fdaa4b60cd5e5fabb3261
tombisho <trpb2@cantab.net>
2021-09-29 12:03:29 -0700

    tweaks to synDS, capturing synDS2 before deleting

--- R/synDS.R
+++ R/synDS.R
@@ -8,22 +8,28 @@
 #' specifying the synthesising method to be used for each variable in the data.
 #' Order of variables is exactly the same as in data.
 #' @param m number of synthetic copies of the original (observed) data to be generated. The default is m = 1.
+#' @param k number of rows to generate
+#' @param proper a logical value with default set to FALSE. If TRUE proper synthesis is conducted.
 #' @param seed an integer to be used as an argument for the set.seed(). If no integer is provided, the defaul
 #' @return synthetic datasets
 #' @author Tom Bishop
 #' @export
 #'
-synDS <- function(data, method = "cart", m=1, seed){
+synDS <- function(data, method, m, k, proper, seed){
   
   data.x <- eval(parse(text=data), envir = parent.frame())
+
+  # if(!is.null(seed)){
+  #   set.seed(seed)
+  # }
   
-  if(!is.null(seed)){
-    set.seed(seed)
+  if(is.null(k)){
+    k = nrow(data.x)
   }
   
-  
-  
-  syn.out <- synthpop::syn(data = data.x, method = method, m = m, seed = seed )
+  syn.out <- synthpop::syn(data = data.x, method = method, m = m, k = k,
+                           proper = proper, drop.not.used = TRUE, drop.pred.only = TRUE,
+                           print.flag = FALSE, seed = seed)
   
   out <- syn.out
   
